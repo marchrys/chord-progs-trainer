a8d921af97f92eb46d07df7e3ade8c5e51e1a499
marchrys <mar.chrysikopoulos@gmail.com>
2021-09-14 18:52:55 +0900

    Ajout des selects pour les réponses.

--- www/js/Training.js
+++ www/js/Training.js
@@ -4,10 +4,17 @@ class Training {
     this.containerDiv = document.getElementById('training');
     this.settings = settings;
     this.chords = [];
+    this.phraseChordsNum = 5;
+    this.answerSelects = null;
 
     this.defineChords();
-    this.displayPreloader();
-    this.checkSoundsLoad();
+    //Au premier chargement, on affiche le preloader
+    if(this.containerDiv.innerHTML == '') {
+      this.displayPreloader();
+      this.checkSoundsLoad();
+    } else {
+      this.createGui();
+    }
   }
 
   defineChords() {
@@ -42,8 +49,42 @@ class Training {
     }.bind(this), 1000);
   }
 
+  initGuiComponents() {
+    //Initialisation des selects
+    const elems = document.querySelectorAll('select');
+    const instances = M.FormSelect.init(elems, {});
+  }
+
   createGui() {
     this.containerDiv.innerHTML = '';
+
+    this.containerDiv.innerHTML += `<div class="row col s11 offset-s1"></div>`;
+    const selectsRow = this.containerDiv.querySelector('.row');
+
+    //Ajout des selects pour les réponses
+    for(let i=1; i<=this.phraseChordsNum; i++) {
+      selectsRow.innerHTML += 
+        `<div class="input-field col s2">
+          <select class="answer-select">
+          </select>
+        </div>`;
+    }
+
+    this.answerSelects = this.containerDiv.querySelectorAll('.answer-select');
+    this.answerSelects.forEach(function(select) {
+      select.disabled = true;
+    });
+    
+    // this.answerSelects.forEach(function(select) {
+    //   this.chords.forEach(function(chord) {
+    //     let option = document.createElement('option');
+    //     option.innerHTML = chord.majorRoman;
+    //     option.value = chord.id;
+    //     select.appendChild(option);
+    //   }.bind(this));
+    // }.bind(this));
+
+    this.initGuiComponents();
   }
 }
 
---
+++ www/js/scales.js
@@ -0,0 +1,19 @@
+const scales = [
+  {
+    id: 1,
+    name: {
+      en: 'major',
+      fr: 'majeure'
+    },
+    intervals: [5, 10, 13, 18, 23, 28]
+  },
+  {
+    id: 2,
+    name: {
+      en: 'harmonic minor',
+      fr: 'mineure harmonique'
+    },
+    intervals: [5, 8, 13, 18, 21, 28]
+  },
+];
+
